04eb835d8ccb467e47987392f552636044fb84e4
An <30442559+epaqu@users.noreply.github.com>
2018-09-04 18:33:42 +0900

    Create move_zeroes.py

---
+++ move_zeroes.py
@@ -0,0 +1,11 @@
+class Solution:
+    def moveZeroes(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: void Do not return anything, modify nums in-place instead.
+        """
+        count = 0
+        for num in nums:
+            if num == 0:
+                nums.remove(0)
+                nums.append(0)
