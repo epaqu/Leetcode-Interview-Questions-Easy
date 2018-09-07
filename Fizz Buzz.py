f2d19b6096937d4b769e96213c9a2ecd526908aa
An <30442559+epaqu@users.noreply.github.com>
2018-09-07 23:43:07 +0900

    Create Fizz Buzz.py
    Write a program that outputs the string representation of numbers from 1 to n. But for multiples of three it should output “Fizz” instead of the number and for the multiples of five output “Buzz”. For numbers which are multiples of both three and five output “FizzBuzz”.

---
+++ Fizz Buzz.py
@@ -0,0 +1,17 @@
+class Solution(object):
+    def fizzBuzz(self, n):
+        """
+        :type n: int
+        :rtype: List[str]
+        """
+        result = []
+        for num in range(1, n+1):
+            if num % 3 == 0 and num % 5 == 0:
+                result.append("FizzBuzz")
+            elif num % 3 == 0:
+                result.append("Fizz")
+            elif num % 5 == 0:
+                result.append("Buzz")
+            else:
+                result.append(str(num))
+        return result
